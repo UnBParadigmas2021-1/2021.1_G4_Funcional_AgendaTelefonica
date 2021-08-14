6f3e60766977f1e03ed1f5d30160cc8732d8c1a7
GabrielHussein <38799575+GabrielHussein@users.noreply.github.com>
2021-08-14 17:24:14 -0300

    Correcao inserção

--- Main.hs
+++ Main.hs
@@ -10,14 +10,15 @@ main = do { putStrLn "Menu Principal da Agenda: ";
         putStrLn "1 - Cadastrar um novo contato";
         putStrLn "2 - Achar um contato";
         putStrLn "3 - Listar todos os contatos";
-        putStrLn "4 - Opções de SMS";
-	    putStrLn "5 - Opções de Email";
+	putStrLn "4 - Opções de Email";
+        putStrLn "5 - Sair da Agenda";
         opcao <- getLine;
-        case opcao of "1" -> subMenuInsere;
+        case opcao of   "1" -> subMenuInsere;
 			"2" -> subMenuAchar;
 			"3" -> subMenuPrintar;
 			"4" -> putStrLn "Fechando agenda";
-			"5" -> putStrLn "Fechando agenda";
+                        "5" -> putStrLn "Fechando agenda";
+
 	}
 
 {--subMenuInsere :: IO()
@@ -40,7 +41,9 @@ subMenuInsere = do
     putStrLn "Insira o e-mail"  
     email <- getLine  
     putStrLn $ "hey " ++ name ++ ", nice to meet you!\n" ++ "Phone: " ++ phone ++ " Email: " ++ email
-    --insereAgenda name phone email
+    let agenda = insereAgenda name phone email
+    putStrLn (show agenda)
+    main
 
 subMenuAchar :: IO()
 subMenuAchar = do { putStrLn "Nome do contato";
